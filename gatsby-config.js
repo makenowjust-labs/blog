0c26aac7807c70e8342ef7baf92a5bd30a69a900
TSUYUSATO Kitsune <make.just.on@gmail.com>
2021-04-02 13:34:15 +0900

    Add syntax higlight by Prism.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,9 +20,13 @@ module.exports = {
       resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
+          "gatsby-remark-katex",
           {
-            resolve: "gatsby-remark-katex",
-          },
+            resolve: "gatsby-remark-prismjs",
+            options: {
+              noInlineHighlight: true,
+            }
+          }
         ],
       },
     },
