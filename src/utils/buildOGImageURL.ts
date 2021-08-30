f536ce306a114c75ef39357f12817425ba3fa8c8
TSUYUSATO Kitsune <make.just.on@gmail.com>
2021-08-30 11:46:35 +0900

    Apply prettier

--- src/utils/buildOGImageURL.ts
+++ src/utils/buildOGImageURL.ts
@@ -8,7 +8,7 @@ const API_ENDPOINT = "https://makenowjust-api-og-image.vercel.app/api/index";
 export const buildOGImageURL = ({ title, info }: OGImageParams) => {
   const params = [
     `title=${encodeURIComponent(title)}`,
-    `info=${encodeURIComponent(info)}`
-  ].join('&');
+    `info=${encodeURIComponent(info)}`,
+  ].join("&");
   return `${API_ENDPOINT}?{params}`;
 };
