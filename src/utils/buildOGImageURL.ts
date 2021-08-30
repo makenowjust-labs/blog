0bcc731b046ea046d1d20ab2de1ab31a621e5e16
TSUYUSATO Kitsune <make.just.on@gmail.com>
2021-08-30 11:29:49 +0900

    Update OGP image URL

--- src/utils/buildOGImageURL.ts
+++ src/utils/buildOGImageURL.ts
@@ -3,10 +3,12 @@ export type OGImageParams = {
   info: string;
 };
 
-const API_ENDPOINT =
-  "https://makenowjust-labo-api-og-image.vercel.app/api/index";
+const API_ENDPOINT = "https://makenowjust-api-og-image.vercel.app/api/index";
 
-export const buildOGImageURL = ({ title, info }: OGImageParams) =>
-  `${API_ENDPOINT}?title=${encodeURIComponent(title)}&info=${encodeURIComponent(
-    info
-  )}`;
+export const buildOGImageURL = ({ title, info }: OGImageParams) => {
+  const params = [
+    `title=${encodeURIComponent(title)}`,
+    `info=${encodeURIComponent(info)}`
+  ].join('&');
+  return `${API_ENDPOINT}?{params}`;
+};
