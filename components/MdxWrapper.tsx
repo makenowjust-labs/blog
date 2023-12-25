802cd64d5e1e2604de53dc22cee8fc1a1503114b
TSUYUSATO Kitsune <make.just.on@gmail.com>
2023-12-25 18:49:50 +0900

    Fix inline code style

--- components/MdxWrapper.tsx
+++ components/MdxWrapper.tsx
@@ -2,7 +2,7 @@ export type Props = React.PropsWithChildren<{}>;
 
 export default function MdxWrapper({ children }: Props) {
   return (
-    <div className="prose-h4:text-md prose prose-stone max-w-full pb-5 prose-h1:border-b-2 prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg prose-code:px-0">
+    <div className="prose-h4:text-md prose prose-stone prose-code:px-2 max-w-full pb-5 prose-h1:border-b-2 prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg prose-code:px-0">
       {children}
     </div>
   );
