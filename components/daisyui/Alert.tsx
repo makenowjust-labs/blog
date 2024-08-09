7b087ed38667ba02be3909f6ec84b87f181116d4
TSUYUSATO Kitsune <make.just.on@gmail.com>
2024-08-09 13:20:19 +0900

    Fix mobile layout

--- components/daisyui/Alert.tsx
+++ components/daisyui/Alert.tsx
@@ -11,8 +11,8 @@ const colorToClassName = {
 
 export default async function Alert({ color, children }: Props) {
   const className = color
-    ? `py-0 my-[1.25em] alert ${colorToClassName[color]}`
-    : "py-0 my-[1.25em] alert";
+    ? `py-0 my-[1.25em] alert text-left ${colorToClassName[color]}`
+    : "py-0 my-[1.25em] alert text-left";
   return (
     <div role="alert" className={className}>
       <div>{children}</div>
--- components/post/MdxWrapper.tsx
+++ components/post/MdxWrapper.tsx
@@ -2,7 +2,7 @@ export type Props = React.PropsWithChildren<{}>;
 
 export default function MdxWrapper({ children }: Props) {
   return (
-    <div className="prose prose-stone max-w-full pb-5 prose-h1:border-b-2 prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg prose-code:px-2">
+    <div className="prose prose-stone max-w-full break-words pb-5 prose-h1:border-b-2 prose-h1:text-2xl prose-h2:text-xl prose-h3:text-lg prose-code:px-2">
       {children}
     </div>
   );
