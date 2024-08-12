c1c1845d70d24c9738ca86cf449e86b1931cc8bb
TSUYUSATO Kitsune <make.just.on@gmail.com>
2024-08-12 20:34:07 +0900

    Use mermaid-cli for rendering mermaid charts

--- src/rehype-mermaid.mjs
+++ src/rehype-mermaid.mjs
@@ -1,9 +1,24 @@
-import { valueToEstree } from "estree-util-value-to-estree";
+import crypto from "crypto";
+import { mkdirSync, existsSync } from "fs";
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
 import { visit, SKIP } from "unist-util-visit";
+import { run } from "@mermaid-js/mermaid-cli";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const ROOT_DIR = path.join(__dirname, "..");
+const NEXT_DIR = path.join(ROOT_DIR, ".next");
+const CACHE_DIR = path.join(NEXT_DIR, "cache/mermaid");
+
+mkdirSync(CACHE_DIR, { recursive: true });
 
 export default function rehypeMermaid() {
-  return (tree) => {
-    let hasMermaid = false;
+  return async (tree) => {
+    const processData = [];
 
     visit(tree, "element", (node, index, parent) => {
       if (
@@ -43,48 +58,21 @@ export default function rehypeMermaid() {
         return;
       }
 
-      hasMermaid = true;
       const source = textNode.value;
 
-      const newTree = {
-        type: "mdxJsxFlowElement",
-        name: "__MermaidRenderer",
-        attributes: [
-          {
-            type: "mdxJsxAttribute",
-            name: "source",
-            value: {
-              type: "mdxJsxAttributeValueExpression",
-              value: "`source`",
-              data: {
-                estree: {
-                  type: "Program",
-                  body: [
-                    {
-                      type: "ExpressionStatement",
-                      expression: valueToEstree(source),
-                    },
-                  ],
-                  sourceType: "module",
-                  comments: [],
-                },
-              },
-            },
-          },
-        ],
-        children: [],
-      };
+      const hash = crypto.createHash("md5").update(source).digest("hex");
+      const input = path.join(CACHE_DIR, `${hash}.mmd`);
+      const output = path.join(CACHE_DIR, `${hash}.svg`);
 
-      parent.children[index] = newTree;
+      processData.push({ source, hash, input, output, parent, index });
 
       return SKIP;
     });
 
-    if (hasMermaid) {
+    if (processData.length > 0) {
       tree.children.push({
         type: "mdxjsEsm",
-        value:
-          'import __MermaidRenderer from "@/components/mermaid/MermaidRenderer"',
+        value: 'import __NextImage from "next/image";',
         data: {
           estree: {
             type: "Program",
@@ -96,14 +84,104 @@ export default function rehypeMermaid() {
                     type: "ImportDefaultSpecifier",
                     local: {
                       type: "Identifier",
-                      name: "__MermaidRenderer",
+                      name: "__NextImage",
                     },
                   },
                 ],
                 source: {
                   type: "Literal",
-                  value: "@/components/mermaid/MermaidRenderer",
-                  raw: '"@/components/mermaid/MermaidRenderer"',
+                  value: "next/image",
+                  raw: '"next/image"',
+                },
+              },
+            ],
+            sourceType: "module",
+            comments: [],
+          },
+        },
+      });
+    }
+
+    for (const { source, hash, input, output, parent, index } of processData) {
+      if (!existsSync(output)) {
+        await fs.writeFile(input, source);
+        await run(input, output, { puppeteerConfig: { headless: "new" } });
+      }
+
+      parent.children[index] = {
+        type: "mdxJsxFlowElement",
+        name: "div",
+        attributes: [
+          {
+            type: "mdxJsxAttribute",
+            name: "className",
+            value: "flex justify-center",
+          },
+        ],
+        data: {
+          _mdxExplicitJsx: true,
+        },
+        children: [
+          {
+            type: "mdxJsxFlowElement",
+            name: "__NextImage",
+            attributes: [
+              {
+                type: "mdxJsxAttribute",
+                name: "src",
+                value: {
+                  type: "mdxJsxAttributeValueExpression",
+                  value: `__Mermaid${hash}`,
+                  data: {
+                    estree: {
+                      type: "Program",
+                      body: [
+                        {
+                          type: "ExpressionStatement",
+                          expression: {
+                            type: "Identifier",
+                            name: `__Mermaid${hash}`,
+                          },
+                        },
+                      ],
+                      sourceType: "module",
+                      comments: [],
+                    },
+                  },
+                },
+              },
+            ],
+            data: {
+              _mdxExplicitJsx: true,
+            },
+            children: [],
+          },
+        ],
+      };
+
+      const relativePath = path.relative(ROOT_DIR, output);
+      tree.children.push({
+        type: "mdxjsEsm",
+        value: `import __Mermaid${hash} from \"@/${relativePath}\";`,
+        data: {
+          estree: {
+            type: "Program",
+            body: [
+              {
+                type: "ImportDeclaration",
+                specifiers: [
+                  {
+                    type: "ImportDefaultSpecifier",
+                    local: {
+                      type: "Identifier",
+                      name: `__Mermaid${hash}`,
+                    },
+                  },
+                ],
+                source: {
+                  type: "Literal",
+                  value: `@/${relativePath}`,
+                  raw: `\"@/${relativePath}\"`,
                 },
               },
             ],
