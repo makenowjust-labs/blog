aeabcd087f7524965f2c349491b672d66f6718c7
TSUYUSATO Kitsune <make.just.on@gmail.com>
2026-01-15 23:24:50 +0900

    Update BLOG_YEAR

--- src/meta.ts
+++ src/meta.ts
@@ -3,7 +3,7 @@ export const BLOG_BASE_URL = "https://makenowjust-labs.github.io/blog";
 export const BLOG_AUTHOR = 'Hiroya Fujinami (a.k.a. TSUYUSATO "MakeNowJust" Kitsune)';
 export const BLOG_DESCRIPTION = "MakeNowJust Laboratory Tech Blog";
 export const BLOG_REPO = "makenowjust-labs/blog";
-export const BLOG_YEAR = "2021-2025";
+export const BLOG_YEAR = "2021-2026";
 export const BLOG_TWITTER = "make_now_just";
 export const BLOG_GITHUB = "makenowjust";
 
